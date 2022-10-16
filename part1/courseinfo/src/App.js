9fd7b96e0431a74d546d0b43e37e65f4810436ca
SquirrelCoder <SquirrelCoder@users.noreply.github.com>
2022-10-16 21:37:21 +0200

    1.5: course information step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,26 +1,28 @@
 function App() {
 
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ];
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header header={course}/>
-            <Content parts={parts}/>
-            <Total parts={parts}/>
+            <Header course={course}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
         </div>
     );
 }
@@ -28,7 +30,7 @@ function App() {
 const Header = (props) => {
     return (
         <div>
-            {props.header}
+            {props.course.name}
         </div>
     );
 };
