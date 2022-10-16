c1b364838287cb3846371fbf986b6fe2965d265a
SquirrelCoder <SquirrelCoder@users.noreply.github.com>
2022-10-16 20:39:07 +0200

    exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,3 @@
-import './App.css';
-
 function App() {
     const course = "Half Stack application development";
     const part1 = "Fundamentals of React";
@@ -11,19 +9,48 @@ function App() {
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header header={course}/>
+
+            <Content part1={part1} exercises1={exercises1}
+                     part2={part2} exercises2={exercises2}
+                     part3={part3} exercises3={exercises3}
+            />
+
+            <Total total={exercises1 + exercises2 + exercises3}/>
         </div>
     );
 }
+
+const Header = (props) => {
+    return (
+        <div>
+            {props.header}
+        </div>
+    );
+};
+
+const Content = (props) => {
+    return (
+        <div>
+            <p>
+                {props.part1} {props.exercises1}
+            </p>
+            <p>
+                {props.part2} {props.exercises2}
+            </p>
+            <p>
+                {props.part3} {props.exercises3}
+            </p>
+        </div>
+    );
+};
+
+const Total = (props) => {
+    return (
+        <div>
+            {props.total}
+        </div>
+    );
+};
 
 export default App;
